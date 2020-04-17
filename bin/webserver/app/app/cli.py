7bbe7bfa9645cf5f2554e44b8944be50afdb0873
Carlos Bailon <cbailon@ugr.es>
2020-04-17 13:33:50 +0200

    Cli command groups updated

--- bin/webserver/app/app/cli.py
+++ bin/webserver/app/app/cli.py
@@ -1,17 +1,18 @@
-import os
 import click
-from flask.cli import AppGroup
 from app import db
 from app.models import User
 
 def register(app):
+    @app.cli.group()
+    def user():
+        """User management commands."""
+        pass
 
-    user_cli = AppGroup('user')
-
-    @user_cli.command("create")
+    @user.command()
     @click.argument("username")
     @click.option("-p", "--password")
-    def create_user(username, password):
+    def create(username, password):
+        """Create a new user."""
         exists = User.query.filter_by(username=username).scalar()
         if exists is None:
             u = User(username=username)
@@ -21,6 +22,3 @@ def register(app):
             click.echo('User '+username+' created')
         else:
             click.echo('Error creating user: username '+username+' already exists')
-
-    app.cli.add_command(user_cli)
-
